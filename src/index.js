d6f046ba7c4a12cd995ff066c6845796c3266b12
Simon Finney <simonfin@ie.ibm.com>
2018-03-21 19:37:51 +0000

    chore(lint-staged): add `lint-staged` configuration

--- src/index.js
+++ src/index.js
@@ -1,29 +1,9 @@
 /**
- * @file Parallax effect using custom properties.
+ * @file Entry point.
  * @author Simon Finney <simonjfinney@gmail.com>
- * @copyright Simon Finney 2018
  */
 
-const NAMESPACE = 'cpp';
+import parallax from './parallax';
 
-/**
- * Finds all of the elements declared as parallax elements and attaches the parallax handler to them.
- */
-function parallax() {
-  [...document.querySelectorAll(`[data-${NAMESPACE}]`)].forEach(element => {
-    const speed = window
-      .getComputedStyle(element)
-      .getPropertyValue(`--${NAMESPACE}-speed`);
-
-    element.style.transform = `translateY(calc(var(--${NAMESPACE}-parallax) * var(--${NAMESPACE}-modifier)))`;
-
-    window.addEventListener('scroll', () => {
-      element.style.setProperty(
-        `--${NAMESPACE}-parallax`,
-        document.documentElement.scrollTop * speed
-      );
-    });
-  });
-}
-
-parallax();
+// Runs the `parallax` method when the DOM content has loaded.
+document.addEventListener('DOMContentLoaded', parallax);
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,16 @@
 /**
  * @file webpack configuration.
  * @author Simon Finney <simonjfinney@gmail.com>
- * @copyright Simon Finney 2018
  */
 
 const path = require('path');
 
 const packageName = require('./package.json').name;
 
-/**
- * Creates a camel-case string from a string.
- * @param {string} str String to convert to camel-case.
- * @returns {string} Camel-case string.
- */
-const toCamelCase = str =>
-  str.replace(/(\-\w)/g, matches => matches[1].toUpperCase());
-
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'lib'),
     filename: `${packageName}.js`,
-    library: toCamelCase(packageName),
   },
 };
